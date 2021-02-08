d21e3b0dd0953b01f132051383fd3a647a5b2486
muborizMirzoev <muboriz10@gmail.com>
2021-02-09 05:50:36 +0700

    feat: solution task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min (array = []) {
+  if (array.length === 0 || !Array.isArray(array)) return 0
+  return Math.min(...array)
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max (array = []) {
+  if (array.length === 0 || !Array.isArray(array)) return 0
+  return Math.max(...array)
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg (array = []) {
+  if (array.length === 0 || !Array.isArray(array)) return 0
+  return array.reduce((acc, curr) => acc + curr)/array.length;
 }
